f18d731eb34105534c7d909a7a66293959a6f3eb
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2021-09-30 13:38:35 -0400

    update github provider dependency

--- providers.tf
+++ providers.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "~> 4.1"
+      version = "~> 4.0"
     }
     tfe = {
       source  = "hashicorp/tfe"
