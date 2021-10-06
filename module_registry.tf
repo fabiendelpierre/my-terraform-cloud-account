7e03828806494fafbb6faf5389c14e372deb787c
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2021-10-06 15:41:03 -0400

    add notes and a repo + registry entry for an Azure virtual network module

--- module_registry.tf
+++ module_registry.tf
@@ -1,3 +1,6 @@
+# This module sets up a blank GitHub repo (using a template or not) and imports
+# it into the TFC private registry, but it needed to be set up "manually"
+# first.
 resource "tfe_registry_module" "github_tfc_registry_module" {
   provider = tfe.module_registry
 
@@ -6,4 +9,20 @@ resource "tfe_registry_module" "github_tfc_registry_module" {
     identifier         = "fabiendelpierre/terraform-github-tfc-registry-module"
     oauth_token_id     = data.tfe_oauth_client.default_oauth_client.oauth_token_id
   }
+}
+
+# The next modules below use the one that was provisioned "manually" above to
+# set up GitHub repos for reusable modules and import them into TFC
+# automatically.
+module "azurerm_virtual_network" {
+  source  = "app.terraform.io/fabiend/tfc-registry-module/github"
+  version = "0.1.0"
+
+  github_repo_name        = "terraform-azurerm-virtual-network"
+  github_repo_description = "A simple module to create a virtual network in Azure with one subnet and a standard route table"
+
+  github_repo_use_template        = true
+  github_repo_template_owner_name = "fabiendelpierre"
+  github_repo_template_repo_name  = "terraform-module-repo-template"
+  oauth_token_id                  = data.tfe_oauth_client.default_oauth_client.oauth_token_id
 }
