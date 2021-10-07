c9ec681535c62424c78006992953adbe76f52c1e
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2021-10-07 17:38:45 -0400

    destroy these so we can recreate them after accidentally deleting the template repo they were based on

--- module_registry.tf
+++ module_registry.tf
@@ -14,32 +14,32 @@ resource "tfe_registry_module" "github_tfc_registry_module" {
 # The next modules below use the one that was provisioned "manually" above to
 # set up GitHub repos for reusable modules and import them into TFC
 # automatically.
-module "azurerm_virtual_network" {
-  depends_on = [module.terraform-module-repo-template]
+# module "azurerm_virtual_network" {
+#   depends_on = [module.terraform-module-repo-template]
 
-  source  = "app.terraform.io/fabiend/tfc-registry-module/github"
-  version = "0.1.0"
+#   source  = "app.terraform.io/fabiend/tfc-registry-module/github"
+#   version = "0.1.0"
 
-  github_repo_name        = "terraform-azurerm-virtual-network"
-  github_repo_description = "A simple module to create a virtual network in Azure with one subnet and a standard route table"
+#   github_repo_name        = "terraform-azurerm-virtual-network"
+#   github_repo_description = "A simple module to create a virtual network in Azure with one subnet and a standard route table"
 
-  github_repo_use_template        = true
-  github_repo_template_owner_name = "fabiendelpierre"
-  github_repo_template_repo_name  = "terraform-module-repo-template"
-  oauth_token_id                  = data.tfe_oauth_client.default_oauth_client.oauth_token_id
-}
+#   github_repo_use_template        = true
+#   github_repo_template_owner_name = "fabiendelpierre"
+#   github_repo_template_repo_name  = "terraform-module-repo-template"
+#   oauth_token_id                  = data.tfe_oauth_client.default_oauth_client.oauth_token_id
+# }
 
-module "azurerm_key_vault" {
-  depends_on = [module.terraform-module-repo-template]
+# module "azurerm_key_vault" {
+#   depends_on = [module.terraform-module-repo-template]
 
-  source  = "app.terraform.io/fabiend/tfc-registry-module/github"
-  version = "0.1.0"
+#   source  = "app.terraform.io/fabiend/tfc-registry-module/github"
+#   version = "0.1.0"
 
-  github_repo_name        = "terraform-azurerm-key-vault"
-  github_repo_description = "A simple module to create an instance of Azure Key Vault"
+#   github_repo_name        = "terraform-azurerm-key-vault"
+#   github_repo_description = "A simple module to create an instance of Azure Key Vault"
 
-  github_repo_use_template        = true
-  github_repo_template_owner_name = "fabiendelpierre"
-  github_repo_template_repo_name  = "terraform-module-repo-template"
-  oauth_token_id                  = data.tfe_oauth_client.default_oauth_client.oauth_token_id
-}
+#   github_repo_use_template        = true
+#   github_repo_template_owner_name = "fabiendelpierre"
+#   github_repo_template_repo_name  = "terraform-module-repo-template"
+#   oauth_token_id                  = data.tfe_oauth_client.default_oauth_client.oauth_token_id
+# }
